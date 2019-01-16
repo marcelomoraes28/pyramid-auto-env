6d4cf1ef63fd8d3486dcbdd7686ec883df16b099
Marcelo Moraes <marcelomoraes@MacBook-Pro-de-Marcelo.local>
2019-01-16 10:51:17 -0200

    Segregating replacement function

--- pyramid_auto_env/__init__.py
+++ pyramid_auto_env/__init__.py
@@ -8,6 +8,23 @@ from pyramid.settings import asbool
 logger = logging.getLogger(__name__)
 
 
+def replace(prefix, *args, **kwargs):
+    """
+    Example:
+          from pyramid_auto_env import replace
+          settings = get_appsettings('development.ini')
+          update_settings = replace('AES', **settings)
+    """
+    for k, v in kwargs.items():
+        transformed_key = "{}_{}".format(prefix.upper(), k.upper().replace('.', '_').replace('-', '_'))
+        envvar_value = os.environ.get(transformed_key)
+        if not envvar_value:
+            continue
+        kwargs[k] = asbool(envvar_value) if envvar_value.lower() in ['true', 'false'] else envvar_value
+        logger.info('Found settings replacement for "{}" at "{}". Setting it up.'.format(k, transformed_key))
+    return kwargs
+
+
 def autoenv_settings(prefix='AES'):
     """
     Example with aes prefix and kwargs:
@@ -28,14 +45,8 @@ def autoenv_settings(prefix='AES'):
     def func_wrapper(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            for k, v in kwargs.items():
-                transformed_key = "{}_{}".format(prefix.upper(), k.upper().replace('.', '_').replace('-', '_'))
-                envvar_value = os.environ.get(transformed_key)
-                if not envvar_value:
-                    continue
-                kwargs[k] = asbool(envvar_value) if envvar_value.lower() in ['true', 'false'] else envvar_value
-                logger.info('Found settings replacement for "{}" at "{}". Setting it up.'.format(k, transformed_key))
-            return func(*args, **kwargs)
+            replace_envs = replace(prefix, *args,**kwargs)
+            return func(*args, **replace_envs)
 
         return wrapper
 
